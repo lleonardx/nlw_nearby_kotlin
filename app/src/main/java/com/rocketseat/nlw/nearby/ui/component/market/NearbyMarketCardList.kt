3c075fb39a4f98f277bd2c30e96979aa9d3901f0
lleonardx <lleonard.alves@gmail.com>
2024-12-13 13:17:28 -0400

    first class using kotlin

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/component/market/NearbyMarketCardList.kt
@@ -0,0 +1,12 @@
+package com.rocketseat.nlw.nearby.ui.component.market
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.rocketseat.nlw.nearby.data.model.Market
+
+@Composable
+fun NearbyMarketCardList(modifier: Modifier = Modifier,
+    markets: List<Market>,
+    onMarketClick: (Market) -> Unit
+) {
+}
